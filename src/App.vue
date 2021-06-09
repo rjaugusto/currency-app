d17a65d73ccff0d8b4053e1dfc198b26fd6e185a
Rafael de Jesus Augusto <rjaugustobe@yahoo.com.br>
2021-06-08 22:28:18 -0300

    Card with currencies informations

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="conteiner grid-lg my-2 py-2">
+    
+  <div class="card">
+    <div class="card-header">
+      <div class="h4">Todas as moedas</div>
+      <div class="card-body">
+        <ListQuotes :quotes="quotes"/>
+      </div>
+    </div>
+  </div>
+
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, reactive, toRefs } from "vue";
+import api from '@/services/api'
+import ListQuotes from './components/ListQuotes'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+
+  components:{ListQuotes},
+
+  setup(){
+
+    const data = reactive({
+      quotes:{}
+    })
+
+
+     onMounted(async () => {
+      const response = await api.all()
+       data.quotes = response.data
+    })
+
+      return {
+        ...toRefs(data)
+        
+        }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/services/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL: 'https://economia.awesomeapi.com.br/json/'
+})
+
+export default {
+
+    all(){
+        return api.get('/all')
+    }
+
+}
